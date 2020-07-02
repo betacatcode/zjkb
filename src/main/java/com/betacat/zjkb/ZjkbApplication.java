56fda12ba51f358de8d8be76fce3e54a8e025aa9
wangxiao <1595990651@qq.com>
2020-07-02 10:27:46 +0800

    初始化

---
+++ src/main/java/com/betacat/zjkb/ZjkbApplication.java
@@ -0,0 +1,13 @@
+package com.betacat.zjkb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjkbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjkbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betacat/zjkb/ZjkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacat.zjkb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjkbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
