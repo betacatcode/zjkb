88dea9606b720afd9e099a71e3e406ef5f9b25b5
wangxiao <1595990651@qq.com>
2020-07-03 09:55:04 +0800

    页面路径修改

--- src/main/java/com/betacat/zjkb/controller/IndexController.java
+++ src/main/java/com/betacat/zjkb/controller/IndexController.java
@@ -8,32 +8,32 @@ public class IndexController {
 
     @RequestMapping(value = {"/","/index","/front/index"})
     public String goIndex(){
-        return "/front/index";
+        return "front/index";
     }
 
     @RequestMapping("/front/about")
     public String goAbout(){
-        return "/front/about";
+        return "front/about";
     }
 
     @RequestMapping("/front/qualifications")
     public String goQualifications(){
-        return "/front/qualifications";
+        return "front/qualifications";
     }
 
     @RequestMapping("/front/works")
     public String goWorks(){
-        return "/front/works";
+        return "front/works";
     }
 
     @RequestMapping("/front/recruit")
     public String goRecruit(){
-        return "/front/recruit";
+        return "front/recruit";
     }
 
     @RequestMapping("/front/contact")
     public String goContact(){
-        return "/front/contact";
+        return "front/contact";
     }
 
 }
